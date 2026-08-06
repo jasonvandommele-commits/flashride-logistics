bab2af0388c310f7b4faad0ba57898c68ed7b778
jasonvandommele-commits <jasonvandommele@icloud.com>
2026-08-06 23:31:56 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,10 +4,42 @@ export default function sitemap() {
       url: "https://flashride-logistics.fr",
       lastModified: new Date(),
     },
+
+    {
+      url: "https://flashride-logistics.fr/transport-express",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://flashride-logistics.fr/tournees-regulieres",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://flashride-logistics.fr/transport-dedie",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://flashride-logistics.fr/transport-marchandises",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://flashride-logistics.fr/logistique-evenementielle",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://flashride-logistics.fr/transport-national",
+      lastModified: new Date(),
+    },
+
     {
       url: "https://flashride-logistics.fr/mentions-legales",
       lastModified: new Date(),
     },
+
     {
       url: "https://flashride-logistics.fr/confidentialite",
       lastModified: new Date(),
