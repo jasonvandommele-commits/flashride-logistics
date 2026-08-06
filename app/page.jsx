a12bb9a1df36e2efdbc4cdf827605a42205cd199
jasonvandommele-commits <jasonvandommele@icloud.com>
2026-08-06 06:45:11 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,60 +2,71 @@ export default function FlashrideLogisticsWebsite() {
   return (
     <main className="bg-white text-gray-900 min-h-screen">
 
-      {/* Navigation */}
-<nav className="fixed top-0 w-full z-50 bg-white border-b border-gray-100 shadow-md text-gray-900">
-
-  <div className="max-w-7xl mx-auto px-4 md:px-8 py-4 flex justify-between items-center">
-
-    <div className="flex items-center gap-1 sm:gap-2 md:gap-4">
-  <img
-    src="/images/IMG_0265.png"
-    alt="Flashride Logistics"
-    className="h-10 md:h-14 w-auto object-contain"
-  />
-
-  <div className="text-xs sm:text-sm md:text-xl font-black tracking-wide whitespace-nowrap">
-  <span className="text-[#0B1F3A]">FLASH</span>
-  <span className="text-orange-500">RIDE</span>
-  <span className="text-[#0B1F3A]"> LOGISTICS</span>
-</div>
-</div>
-
+            {/* Navigation */}
+      <nav className="fixed top-0 w-full z-50 bg-white border-b border-gray-100 shadow-md text-gray-900">
+      
+        <div className="max-w-7xl mx-auto px-4 md:px-8 py-4 flex justify-between items-center">
+      
+      
+          {/* Logo + Nom */}
+          <div className="flex items-center gap-1 sm:gap-2 md:gap-4">
+      
+            <img
+              src="/images/IMG_0265.png"
+              alt="Flashride Logistics"
+              className="h-10 md:h-14 w-auto object-contain"
+            />
+      
+            <div className="text-xs sm:text-sm md:text-xl font-black tracking-wide whitespace-nowrap">
+              <span className="text-[#0B1F3A]">FLASH</span>
+              <span className="text-orange-500">RIDE</span>
+              <span className="text-[#0B1F3A]"> LOGISTICS</span>
+            </div>
+      
+          </div>
+      
+      
+          {/* Menu PC */}
           <div className="hidden md:flex gap-8 items-center text-sm font-medium">
+      
             <a href="#" className="hover:text-orange-500 transition">
               Accueil
             </a>
-
+      
             <a href="#services" className="hover:text-orange-500 transition">
               Services
             </a>
-
+      
             <a href="#about" className="hover:text-orange-500 transition">
               Entreprise
             </a>
-
+      
             <a href="#contact" className="hover:text-orange-500 transition">
               Contact
             </a>
-
+      
             <a
               href="#devis"
               className="bg-orange-500 px-5 py-3 rounded-xl font-bold hover:bg-orange-600"
             >
               Demander un devis
             </a>
+      
           </div>
-
-          </div>
-
+      
+      
           {/* Menu mobile */}
           <div className="md:hidden">
-            <button className="text-gray-900 text-3xl">
+      
+            <button className="text-gray-900 text-3xl font-bold">
               ☰
             </button>
+      
           </div>
-
+      
+      
         </div>
+      
       </nav>
 
 
