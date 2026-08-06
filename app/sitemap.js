c4c8036303c825c9472cae59754341ea3e72e54b
jasonvandommele-commits <jasonvandommele@icloud.com>
2026-08-06 20:22:54 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,1 +1,16 @@
-
+export default function sitemap() {
+  return [
+    {
+      url: "https://flashride-logistics.fr",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://flashride-logistics.fr/mentions-legales",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://flashride-logistics.fr/confidentialite",
+      lastModified: new Date(),
+    },
+  ];
+}
