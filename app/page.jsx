908c4ff1ce3d4f66d0b6da539fe6f1f177b85ff9
jasonvandommele-commits <jasonvandommele@icloud.com>
2026-08-06 04:04:17 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function FlashrideLogisticsWebsite() {
   <img
     src="/images/IMG_0265.png"
     alt="Flashride Logistics"
-    className="h-12 w-auto"
+    className="h-20 w-auto object-contain"
   />
           </div>
 
