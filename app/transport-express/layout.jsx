808108c092b03b01cc0e0a79f9e934c7f5648c29
jasonvandommele-commits <jasonvandommele@icloud.com>
2026-08-06 23:24:31 +0100

    Update layout.jsx

--- app/transport-express/layout.jsx
+++ app/transport-express/layout.jsx
@@ -1,1 +1,10 @@
+export const metadata = {
+  title: "Transport express en Île-de-France et Europe | Flashride Logistics",
+  description:
+    "Flashride Logistics propose un service de transport express pour les livraisons urgentes en Île-de-France, en France et en Europe.",
+};
 
+
+export default function Layout({ children }) {
+  return children;
+}
