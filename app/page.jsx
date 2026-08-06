e2625f72b819685b887fe06b9f271f06d197a72a
jasonvandommele-commits <jasonvandommele@icloud.com>
2026-08-06 06:06:06 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -302,7 +302,8 @@ export default function FlashrideLogisticsWebsite() {
 
           </div>
 
-
+          <form className="bg-white rounded-3xl shadow-xl p-8 grid md:grid-cols-2 gap-6">
+          
           <select
             className="border rounded-xl p-4"
            >
